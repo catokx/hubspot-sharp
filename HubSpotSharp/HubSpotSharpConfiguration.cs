1c963d800386c27298307afeffce4fc15a7cf82b
edeamicis <e.deamicis@callistosoftware.com>
2016-12-01 16:49:40 +0100

    - project init with api key configuration get/set

---
+++ HubSpotSharp/HubSpotSharpConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Net.Http;
+using System.Configuration;
+
+namespace HubSpotSharp
+{
+    public class HubSpotSharpConfiguration
+    {
+        public string ApiKey
+        {
+            get
+            {
+                try
+                {
+                    return System.Configuration.ConfigurationManager.AppSettings["StripeApiKey"];
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            set
+            {
+                System.Configuration.ConfigurationManager.AppSettings["StripeApiKey"] = value;
+            }
+        }
+
+    }
+}
